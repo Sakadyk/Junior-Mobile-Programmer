4f631eff73a1804e00262675909d2890b83e2e37
Ananda Nurrizki <Ananda_Nurrizki@yahoo.com>
2023-06-26 21:25:39 +0700

    update SignUp

--- app/src/main/java/com/sugiartha/juniorandroid/SignupActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/SignupActivity.java
@@ -6,39 +6,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
     Spinner spin;
     String spin_val;
     String[] gender = { "Laki-Laki", "Perempuan" };
-    //array of strings used to populate the spinner
+
+    EditText etPassword;
+    EditText etConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        spin = (Spinner) findViewById(R.id.spinner_id);//fetching view's id
-        // Register a callback to be invoked when an item in this AdapterView has been selected
+        spin = (Spinner) findViewById(R.id.spinner_id);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+
         spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                       int  position, long id) {
-                // TODO Auto-generated method stub
-                spin_val = gender[position];//saving the value selected
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+                spin_val = gender[position];
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
             }
-        });//setting array adaptors to spinners
-        // ArrayAdapter is a BaseAdapter that is backed by an array of arbitrary objects
+        });
 
         ArrayAdapter<String> spin_adapter = new ArrayAdapter<String>(SignupActivity.this, android.R.layout.simple_spinner_item, gender);
-        // setting adapters to spinners
         spin.setAdapter(spin_adapter);
+
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = etPassword.getText().toString();
+                String confirmPassword = etConfirmPassword.getText().toString();
+
+                if (password.isEmpty() || confirmPassword.isEmpty()) {
+                    // Ada field yang kosong, tampilkan pesan kesalahan
+                    Toast.makeText(SignupActivity.this, "Harap lengkapi semua field", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    // Konfirmasi password tidak cocok, tampilkan pesan kesalahan
+                    Toast.makeText(SignupActivity.this, "Konfirmasi password tidak cocok", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Lanjutkan dengan proses pendaftaran
+                    // ...
+                }
+            }
+        });
     }
 }
+
+
+
