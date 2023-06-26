87f33acfe436d8dcc4e722ed58394b1cf3be07ab
Sakadyk <sakadyk69@gmail.com>
2023-06-26 21:17:27 +0700

    Patch 1.1 - Splash Screen

--- app/src/main/java/com/sugiartha/juniorandroid/SplashActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/SplashActivity.java
@@ -5,24 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-    Handler handler;
+    ProgressBar progressBar;
+    TextView textView;
+    int value;
+    Handler handler=new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        progressBar = findViewById(R.id.progressbar);
+        textView = findViewById(R.id.progresstext);
 
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                startProgress();
             }
-        },3000);
+        });
+        thread.start();
+    }
 
+    public void startProgress() {
+        for (value = 0; value < 100; value = value+1) {
+            try {
+                Thread.sleep(25);
+                progressBar.setProgress(value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(String.valueOf(value+"%"));
+                }
+            });
+        }
+        run();
+    }
+    public void run() {
+        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
