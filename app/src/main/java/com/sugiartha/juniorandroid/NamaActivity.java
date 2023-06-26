8f00afd23bdd4655e311cd12cc82afc326c8864d
Abizarhaikal <abizarhaikal@gmail.com>
2023-06-27 00:16:13 +0700

    final nama

--- app/src/main/java/com/sugiartha/juniorandroid/NamaActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/NamaActivity.java
@@ -14,6 +14,8 @@ public class NamaActivity extends AppCompatActivity {
     EditText editNama;
     TextView txtHasil;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
