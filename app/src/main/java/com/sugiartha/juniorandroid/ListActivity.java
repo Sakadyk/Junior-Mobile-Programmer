98563afd6cd8826e71860a46ab07adf14b0b7bb5
Ananda Nurrizki <Ananda_Nurrizki@yahoo.com>
2023-06-26 18:07:00 +0700

    update list

--- app/src/main/java/com/sugiartha/juniorandroid/ListActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/ListActivity.java
@@ -3,44 +3,69 @@ package com.sugiartha.juniorandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
 
-    //mendeklarasikan listview var dan menginisialasi array tipe data string
     private ListView lvItem;
     private String[] namanegara = new String[]{
-            "Indonesia","Malaysia","Singapore" ,
+            "Indonesia", "Malaysia", "Singapore",
             "Italia", "Inggris", "Belanda",
-            "Argentina","Chile",
-            "Mesir", "Uganda"};
+            "Argentina", "Chile",
+            "Mesir", "Uganda", "Thailand", "Filipina",
+            "Vietnam", "Amerika", "China", "Jepang",
+            "Korea"};
+
+    private int[] bendera = new int[]{
+            R.drawable.indonesia, R.drawable.malaysia, R.drawable.singapore,
+            R.drawable.italia, R.drawable.inggris, R.drawable.belanda,
+            R.drawable.argentina, R.drawable.chile,
+            R.drawable.mesir, R.drawable.uganda,R.drawable.thailand,
+            R.drawable.filipina, R.drawable.vietnam, R.drawable.amerika,
+            R.drawable.china, R.drawable.jepang, R.drawable.korea};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        getSupportActionBar().setTitle("ListView Sederhana"); //tampil judul
+        getSupportActionBar().setTitle("ListView Sederhana");
 
-        //Membinding atau memformat data
         lvItem = (ListView) findViewById(R.id.list_view);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListActivity.this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
 
-        //menset data di dalam listview
         lvItem.setAdapter(adapter);
 
         lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(ListActivity.this,
-                        "Memilih : "+namanegara[position], Toast.LENGTH_LONG).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LayoutInflater inflater = getLayoutInflater();
+                View toastLayout = inflater.inflate(R.layout.toast_layout,
+                        (ViewGroup) findViewById(R.id.toast_root));
+
+                ImageView imageView = toastLayout.findViewById(R.id.toast_image);
+                TextView textView = toastLayout.findViewById(R.id.toast_text);
+
+                imageView.setImageResource(bendera[position]);
+                textView.setText("Memilih : " + namanegara[position]);
+
+                Toast toast = new Toast(ListActivity.this);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(toastLayout);
+                toast.show();
             }
         });
     }
 }
+
+
