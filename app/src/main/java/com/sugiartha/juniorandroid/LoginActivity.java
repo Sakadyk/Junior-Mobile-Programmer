2c1ddc5f10c6ebc682cafcaa1947ebabc7bc20d5
Kevin Grimaldi <kevin@gmail.com>
2023-07-08 21:36:09 +0700

    Telah menambahkan patch update link direct ke halaman signup dan front-end

--- app/src/main/java/com/sugiartha/juniorandroid/LoginActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/LoginActivity.java
@@ -2,13 +2,30 @@ package com.sugiartha.juniorandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView linkTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView linkTextView = findViewById(R.id.link);
+        linkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Arahkan pengguna ke halaman lain
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
